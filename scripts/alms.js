78726edcec1795b2984900ac6aae21351d9cfba3
賴宇衎-資訊部-證券 <yukan.lai@sinopac.com>
2019-08-27 18:38:41 +0800

    [Add] blockUI [Modify] login

--- scripts/alms.js
+++ scripts/alms.js
@@ -49,42 +49,28 @@ $(document).ready(function() {
     });
   }
 
-  // paperDashboard.initMinimizeSidebar();
-
   $navbar = $('.navbar[color-on-scroll]');
   scroll_distance = $navbar.attr('color-on-scroll') || 500;
 
   // Check if we have the class "navbar-color-on-scroll" then add the function to remove the class "navbar-transparent" so it will transform to a plain color.
-  if ($('.navbar[color-on-scroll]').length != 0) {
+ /*  if ($('.navbar[color-on-scroll]').length != 0) {
     paperDashboard.checkScrollForTransparentNavbar();
     $(window).on('scroll', paperDashboard.checkScrollForTransparentNavbar)
-  }
+  } */
 
   $('.form-control').on("focus", function() {
     $(this).parent('.input-group').addClass("input-group-focus");
   }).on("blur", function() {
     $(this).parent(".input-group").removeClass("input-group-focus");
   });
-
-  // Activate bootstrapSwitch
-  // $('.bootstrap-switch').each(function() {
-  //   $this = $(this);
-  //   data_on_label = $this.data('on-label') || '';
-  //   data_off_label = $this.data('off-label') || '';
-
-  //   $this.bootstrapSwitch({
-  //     onText: data_on_label,
-  //     offText: data_off_label
-  //   });
-  // });
 });
 
 $(document).on('click', '.navbar-toggle', function() {
   $toggle = $(this);
 
-  if (paperDashboard.misc.navbar_menu_visible == 1) {
+  if (alms.misc.navbar_menu_visible == 1) {
     $('html').removeClass('nav-open');
-    paperDashboard.misc.navbar_menu_visible = 0;
+    $.alms.misc.navbar_menu_visible = 0;
     setTimeout(function() {
       $toggle.removeClass('toggled');
       $('#bodyClick').remove();
@@ -98,7 +84,7 @@ $(document).on('click', '.navbar-toggle', function() {
     div = '<div id="bodyClick"></div>';
     $(div).appendTo('body').click(function() {
       $('html').removeClass('nav-open');
-      paperDashboard.misc.navbar_menu_visible = 0;
+      $.alms.misc.navbar_menu_visible = 0;
       setTimeout(function() {
         $toggle.removeClass('toggled');
         $('#bodyClick').remove();
@@ -106,38 +92,17 @@ $(document).on('click', '.navbar-toggle', function() {
     });
 
     $('html').addClass('nav-open');
-    paperDashboard.misc.navbar_menu_visible = 1;
+    $.alms.misc.navbar_menu_visible = 1;
   }
 });
 
 $(document).on('click', '.menu-toggle', function() {
-  $toggler = $(this);
-
-  if (paperDashboard.misc.navbar_menu_shrink == 1) {
+  if ($.alms.misc.navbar_menu_shrink == 1) {
     $('html').removeClass('menu-shrink');
-    paperDashboard.misc.navbar_menu_shrink = 0;
-    // setTimeout(function() {
-    //   $toggle.removeClass('toggled');
-    //   $('#bodyClick').remove();
-    // }, 550);
-
+    $.alms.misc.navbar_menu_shrink = 0;
   } else {
-    // setTimeout(function() {
-    //   $toggle.addClass('toggled');
-    // }, 580);
-
-    // div = '<div id="bodyClick"></div>';
-    // $(div).appendTo('body').click(function() {
-    //   $('html').removeClass('nav-open');
-    //   paperDashboard.misc.navbar_menu_visible = 0;
-    //   setTimeout(function() {
-    //     $toggle.removeClass('toggled');
-    //     $('#bodyClick').remove();
-    //   }, 550);
-    // });
-
     $('html').addClass('menu-shrink');
-    paperDashboard.misc.navbar_menu_shrink = 1;
+    $.alms.misc.navbar_menu_shrink = 1;
   }
 });
 
@@ -156,71 +121,381 @@ $(window).resize(function() {
   }
 });
 
-paperDashboard = {
+$.alms = {
   misc: {
     navbar_menu_visible: 0,
     navbar_menu_shrink: 0
-  },
+	},
 
-  // initMinimizeSidebar: function() {
-  //   if ($('.sidebar-mini').length != 0) {
-  //     sidebar_mini_active = true;
-  //   }
+	block: function($ele, msg){
+		message = "Processing..";
+		if(msg){
+			message = msg;
+		}
 
-  //   $('#minimizeSidebar').click(function() {
-  //     var $btn = $(this);
+		var options = {
+			baseZ: 1040,
+			overlayCss: {
+				backgroundColor: '#b6b6b6',
+				opacity: .6
+			},
+			css:{
+				border: 'none',
+				padding: '15px',
+				backgroundColor: '#4d8181',
+				'-webkit-border-radius': '3px',
+				'-moz-border-radius': '3px',
+				color: '#d3d3d3'
+			}
+		};
 
-  //     if (sidebar_mini_active == true) {
-  //       $('body').addClass('sidebar-mini');
-  //       sidebar_mini_active = true;
-  //       paperDashboard.showSidebarMessage('Sidebar mini activated...');
-  //     } else {
-  //       $('body').removeClass('sidebar-mini');
-  //       sidebar_mini_active = false;
-  //       paperDashboard.showSidebarMessage('Sidebar mini deactivated...');
-  //     }
+		if($ele && $ele instanceof jQuery){
+			if($(window).width() <= 320 || $ele.width <= 320){
+				message = "";
+			}
+			$.extend(options, {message: message});
+			$ele.block(options);
+		}else if(typeof $ele === 'string'){
+			if($(window).width() <= 320 || $($ele).width() <= 320){
+				message = "";
+			}
+			$.extend(options, {message: message});
+			$($ele).block(options);
+		}else{
+			if($(window).width() <= 320){
+				message = "";
+			}
+			$.extend(options, {message: message});
+			$.blockUI(options);
+		}
 
-  //     // we simulate the window Resize so the charts will get updated in realtime.
-  //     var simulateWindowResize = setInterval(function() {
-  //       window.dispatchEvent(new Event('resize'));
-  //     }, 180);
-
-  //     // we stop the simulation of Window Resize after the animations are completed
-  //     setTimeout(function() {
-  //       clearInterval(simulateWindowResize);
-  //     }, 1000);
-  //   });
-  // },
-
-  // showSidebarMessage: function(message) {
-  //   try {
-  //     $.notify({
-  //       icon: "now-ui-icons ui-1_bell-53",
-  //       message: message
-  //     }, {
-  //       type: 'info',
-  //       timer: 4000,
-  //       placement: {
-  //         from: 'top',
-  //         align: 'right'
-  //       }
-  //     });
-  //   } catch (e) {
-  //     console.log('Notify library is missing, please make sure you have the notifications library added.');
-  //   }
-
-  // }
-
+	},
+	unblock: function($ele){
+		if($ele && $ele instanceof jQuery){
+			$ele.unblock();
+		}else if(typeof $ele === 'string'){
+			$($ele).unblock();
+		}else{
+			$.unblockUI();
+		}
+	}
 };
 
-function hexToRGB(hex, alpha) {
-  var r = parseInt(hex.slice(1, 3), 16),
-    g = parseInt(hex.slice(3, 5), 16),
-    b = parseInt(hex.slice(5, 7), 16);
 
-  if (alpha) {
-    return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
-  } else {
-    return "rgb(" + r + ", " + g + ", " + b + ")";
+$.alms.popup = {
+  defaultSetting: {
+    placement: {
+      from: "bottom",
+      align: "center"
+    },
+    offset: {
+      x: 20,
+      y: 500
+		},
+    animate: {
+      enter: 'animated zoomIn',
+      exit: 'animated fadeOut'
+    }
+  },
+  success: function(message){
+		var custom = {
+			type: "success"
+		};
+		$.alms.popup.invoke(message, custom);
+  },
+  error: function(message){
+		var custom = {
+			type: "danger"
+		};
+		$.alms.popup.invoke(message, custom);
+  },
+  warn: function(message){
+		var custom = {
+			type: "warning"
+		};
+		$.alms.popup.invoke(message, custom);
+  },
+  info: function(message){
+		var custom = {
+			type: "info"
+		};
+		$.alms.popup.invoke(message, custom);
+  },
+  primary: function(message){
+		var custom = {
+			type: "primary"
+		};
+		$.alms.popup.invoke(message, custom);
+  },
+  invoke: function(message, customSetting){
+	  var settings = $.extend({}, $.alms.popup.defaultSetting, customSetting);
+	  $.notify({
+			message: message
+	  }, settings);
   }
-}
+};
+
+
+/* $.sinopac.message = {
+	defaultSetting: {
+		inEffect: { opacity: 'show' },
+		inEffectDuration: 600,
+		stayTime: 5000,
+		text: '',
+		sticky: false,
+		// notice, warn, error, success
+		type: 'success',
+		// showing time
+		timeStamp: new Date(),
+		// top-left, top-center, top-right, 
+		// middle-left, middle-center, middle-right
+		position: 'middle-center',
+		// callback function
+		close: null
+	},
+
+	boxes: new Array(),
+
+	error: function (text, close) {
+		if (text != 'abort' && typeof (text) != 'undefined') {
+			return $.sinopac.message.invoke('error', text, true, close);
+		}
+	},
+
+	notice: function (text, close) {
+		return $.sinopac.message.invoke('notice', text, true, close);
+	},
+
+	warn: function (text, close) {
+		return $.sinopac.message.invoke('warn', text, true, close);
+	},
+
+	success: function (text, close) {
+		return $.sinopac.message.invoke('success', text, false, close);
+	},
+
+	confirm: function (text, clost) {
+		return $.sinopac.message.invoke('confirm', text, true, close);
+	},
+
+	invoke: function (type, text, sticky, close) {
+		var settings = {
+			type: type,
+			text: text,
+			timeStamp: new Date(),
+			sticky: sticky,
+			close: close
+		};
+
+		return $.sinopac.message.show(settings);
+	},
+
+	show: function (options) {
+		var settings = {};
+		$.extend(settings, $.sinopac.message.defaultSetting, options);
+
+		if (settings.type === 'notice' || settings.type === 'confirm') {
+			settings.sticky = true;
+		}
+
+		var df = $.Deferred();
+
+		try {
+			if (settings.text.length == 0) {
+				switch (settings.type) {
+					case 'success':
+						settings.text = '處理成功';
+						break;
+					case 'error':
+					case 'notice':
+					case 'confirm':
+					case 'warn':
+					default:
+						settings.text = [
+							'系統發生錯誤，請聯絡資訊處<br />',
+							'ParentTid: ',
+							sinopac.parentTid
+						].join('');
+						break;
+				}
+			}
+		} catch (e) {
+			settings.text = e.message;
+		}
+
+		$.sinopac.message.boxes.push(settings);
+
+		var wrap = !$('.message-container').length
+			? $('<div>')
+				.addClass('message-container')
+				.addClass('message-position-' + settings.position)
+				.appendTo('body')
+			: $('.message-container');
+		var outer = $('<div>').addClass('message-item-wrapper');
+		var inner = $('<div>')
+			.hide()
+			.addClass('message-item message-type-' + settings.type)
+			.appendTo(wrap)
+			.html($('<p>').append(settings.text))
+			.animate(settings.inEffect, settings.inEffectDuration)
+			.wrap(outer);
+
+		if (settings.type === 'notice') {
+			$('<p>')
+				.html($('<button>')
+					.prop('type', 'button')
+					.addClass('btn btn-default')
+					.html('確定')
+					.on(sinopac.click, function () {
+						$.sinopac.message.remove(inner, settings);
+					})
+				)
+				.appendTo(inner);
+		} else if (settings.type === 'confirm') {
+			if (settings.close) {
+				settings.close = null;
+			}
+			$.blockUI({ message: '' });
+			var btnYes = $('<button>')
+					.prop('type', 'button')
+					.addClass('btn btn-default')
+					.html('確定')
+					.on(sinopac.click, function () {
+						$.sinopac.message.remove(inner, settings);
+						$.unblockUI('');
+						df.resolve();
+					});
+			var btnNo = $('<button>')
+					.prop('type', 'button')
+					.addClass('btn btn-default')
+					.html('取消')
+					.on(sinopac.click, function () {
+						$.sinopac.message.remove(inner, settings);
+						$.unblockUI('');
+						df.reject();
+					});
+
+			$('<p>')
+				.append(btnYes)
+				.append(' ')
+				.append(btnNo)
+				.appendTo(inner);
+		} else {
+			$('<div>')
+				.addClass('message-item-close')
+				.prependTo(inner)
+				.on(sinopac.click, function () {
+					$.sinopac.message.remove(inner, settings);
+				});
+		}
+		if (settings.type === 'warn') {
+			var timer;
+			timer = setTimeout(
+				function () {
+					var handler = function (e) {
+						$.sinopac.message.remove(inner, settings);
+						$(document).unbind(sinopac.click, handler);
+					};
+					$(document).bind(sinopac.click, handler);
+				},
+				settings.stayTime
+			)
+
+			var mouseIn = function (e) {
+				clearTimeout(timer);
+			}
+
+			var mouseOut = function (e) {
+				timer = setTimeout(function () {
+					var handler = function (e) {
+						$.sinopac.message.remove(inner, settings);
+						$(document).unbind(sinopac.click, handler);
+					};
+					$(document).bind(sinopac.click, handler);
+				}, settings.stayTime)
+			}
+
+			$('.message-type-warn')
+				.unbind('mouseenter', mouseIn)
+				.unbind('mouseleave', mouseOut)
+				.bind('mouseenter', mouseIn)
+				.bind('mouseleave', mouseOut)
+		}
+		$('<div>')
+			.addClass('message-item-image')
+			.addClass('message-item-image-' + settings.type)
+			.prependTo(inner);
+
+		if (navigator.userAgent.match(/MSIE 6/i)) {
+			wrap.css({ top: document.documentElement.scrollTop });
+		}
+
+		if (!settings.sticky) {
+			setTimeout(
+				function () {
+					$.sinopac.message.remove(inner, settings);
+				},
+				settings.stayTime
+			);
+		}
+
+		if (settings.type === 'confirm') {
+			return df;
+		} else {
+			return inner;
+		}
+	},
+
+	remove: function (obj, options) {
+		obj.animate(
+			{ opacity: '0' },
+			600,
+			function () {
+				obj.parent().animate(
+					{ height: '0px' },
+					300,
+					function () { obj.parent().remove(); }
+				);
+
+				switch (options.type) {
+					case 'success':
+						$.sinopac.log.info(options.text);
+						break;
+					case 'error':
+					case 'notice':
+					case 'confirm':
+						$.sinopac.log.error(options.text);
+						break;
+					case 'warn':
+						$.sinopac.log.warn(options.text);
+						break;
+					default:
+						$.sinopac.log.debug(options.text);
+						break;
+				}
+
+				if (options && options.close !== null) {
+					options.close();
+				}
+			}
+		);
+	},
+
+	removeAll: function () {
+		var that = this;
+		$('.message-item:not(.history-message)').each(function (index, element) {
+			var msgClass = $(element).prop('class');
+			var type = msgClass.replace('message-item message-type-', '');
+			var text = $(element)
+				.children('p')
+				.first()
+				.html();
+			var options = {
+				type: type,
+				text: text,
+				close: null
+			}
+			that.remove($(element), options);
+		});
+	}
+} */
--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,34 @@
-login = {
-    loginAjax: function(){
-        
+account = {
+    login: function(user, password){
+        try{
+            if(!user && !password){
+                // validation 
+                $alms.popup.error("ID or Password input error");
+                return;
+            }
+
+            var data = {
+                "user": user,
+                "password": password
+            };
+
+            $.ajax({
+                url: "https://localhost:5001/api/Sign/in",
+                type: 'POST',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(data),
+                beforeSend: function(){},
+                success: function(data, status, xhr){
+                    console.log("apiData:", data, status, xhr);
+                },
+                fail: function(jqXhr, textStatus, errorMessage){
+                    console.log("apiFail:", jqXhr, textStatus, errorMessage);
+                }
+            });
+        }
+        catch(err){
+            console.log("apiErr:", err);
+        }
     }
 }
