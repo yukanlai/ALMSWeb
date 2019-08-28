e1b9eb68342f55929b2b8cbeb8751d4a701b9ab8
賴宇衎-資訊部-證券 <yukan.lai@sinopac.com>
2019-08-28 11:24:50 +0800

    [Modify] replace CDN links with files

--- scripts/login.js
+++ scripts/login.js
@@ -13,21 +13,29 @@ account = {
             };
 
             $.ajax({
-                url: "https://localhost:5001/api/Sign/in",
                 type: 'POST',
+                url: "https://localhost:5001/api/Sign/in",
+                async: true,
                 contentType: 'application/json',
                 dataType: 'json',
                 data: JSON.stringify(data),
-                beforeSend: function(){},
+                beforeSend: function(){
+                    $.alms.block();
+                },
                 success: function(data, status, xhr){
+                    // redirect
                     console.log("apiData:", data, status, xhr);
                 },
-                fail: function(jqXhr, textStatus, errorMessage){
+                error: function(jqXhr, textStatus, errorMessage){
                     console.log("apiFail:", jqXhr, textStatus, errorMessage);
+                },
+                complete: function(){
+                    $.alms.unblock();
                 }
             });
         }
         catch(err){
+            $.alms.unblock();
             console.log("apiErr:", err);
         }
     }
